8f6fc029e8a73dd1e9d8e766387afc7893b7c010
Jovana Cerovac <jovanacerovac004@gmail.com>
2025-07-03 01:06:21 +0200

    Add unit test for fetching choices

---
+++ backend/ChoiceAPI/ChoiceAPI.Core.UnitTests/Services/ChoiceServiceTests.cs
@@ -0,0 +1,41 @@
+using ChoiceAPI.Core.Contracts;
+using ChoiceAPI.Core.Domain;
+using ChoiceAPI.Core.Persistence;
+using ChoiceAPI.Core.Services;
+using ChoiceAPI.Core.Services.Abstractions;
+using NSubstitute;
+
+namespace ChoiceAPI.Core.UnitTests.Services;
+
+public class ChoiceServiceTests
+{
+    private readonly IChoiceRepository _choiceRepository;
+    private readonly IRandomNumberService _randomNumberService;
+    private readonly ChoiceService _choiceService;
+
+    public ChoiceServiceTests()
+    {
+        _choiceRepository = Substitute.For<IChoiceRepository>();
+        _randomNumberService = Substitute.For<IRandomNumberService>();
+        _choiceService = new ChoiceService(_choiceRepository, _randomNumberService);
+    }
+
+    [Fact]
+    public void GetAll_ReturnsAllChoices()
+    {
+        // Arrange
+        var choices = new List<Choice>
+        {
+            new RockChoice(),
+            new ScissorsChoice()
+        };
+        _choiceRepository.GetAllChoices().Returns(choices);
+
+        // Act
+        var result = _choiceService.GetAll().ToList();
+
+        // Assert
+        Assert.Equal(choices.Count, result.Count);
+        Assert.All(result, r => Assert.IsType<ChoiceResponse>(r));
+    }
+}
